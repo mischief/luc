1c06aefab54ab224f97260c70244e081d10368db
mischief <mischief@offblast.org>
2012-06-28 21:46:14 +0000

    working irc bot

---
+++ config.lua
@@ -0,0 +1,13 @@
+local info = {
+  nick = "luc",
+  username = "luc",
+  realname = "luc",
+  server = "irc.hackthissite.org",
+  channels = { "#bots", "#C++" },
+  
+  char = ".",
+
+  version = "luc v1.0"
+}
+
+return info
